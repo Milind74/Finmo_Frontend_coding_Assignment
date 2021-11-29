dfeed10cf250ffcdba91f75605bb8b93771ee978
Milind <milindanand74@gmail.com>
2021-11-29 12:47:04 +0530

    logo changed

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import Navbar from "./Navbarpage";
 import { useDispatch, useSelector } from "react-redux";
 import { getData } from "../Redux/action";
@@ -6,17 +6,16 @@ import styles from "../Components/styles/styles.module.css";
 import Modal from "@mui/material/Modal";
 
 const Dashboard = () => {
-  const [open, setOpen] = useState(false);//hook for modal
-  const [image, setImage] = useState("");//hook for modal
-  const state = useSelector((state) => state.PhotosData.results);// useSelector hook used to select  the data from store
-  const dispatch = useDispatch();//deispath function used to dispatch an action over here
+  const [open, setOpen] = useState(false); //hook for modal
+  const [image, setImage] = useState(""); //hook for modal
+  const state = useSelector((state) => state.PhotosData.results); // useSelector hook used to select  the data from store
+  const dispatch = useDispatch(); //deispath function used to dispatch an action over here
 
   //dispatching an action over here inside useffect so that it would rended after component mount
   useEffect(() => {
     dispatch(getData());
   }, [dispatch]);
 
-  
   //modal logic
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -26,14 +25,32 @@ const Dashboard = () => {
     handleOpen();
   };
 
+
+  //logic for icons
+
+  const handleicon=useRef(null)
+
+  const icontoggle=()=>{
+    if(handleicon.current.style.color==="red"  ){
+   handleicon.current.style.color="white" 
+  }
+  else{
+    handleicon.current.style.color="red" 
+
+  }
+}
+
+
+
+  
   return (
     <>
-    {/* //calling navbar over here */}
+      {/* //calling navbar over here */}
       <div>
         <Navbar />
       </div>
 
-{/* //using state to map and display the data */}
+      {/* //using state to map and display the data */}
       <div className={styles.parentcont}>
         {state &&
           state.map((item) => {
@@ -50,7 +67,7 @@ const Dashboard = () => {
             );
           })}
       </div>
-{/* //logic for modal written over here */}
+      {/* //logic for modal written over here */}
       <Modal
         open={open}
         onClose={handleClose}
@@ -58,7 +75,12 @@ const Dashboard = () => {
         aria-describedby="modal-modal-description"
       >
         <div className={styles.modal}>
-          <img src={image} alt="loading"></img>
+          <div className={styles.loadingimg}>
+            <img src={image} alt="loading"></img>
+          </div>
+          <div ref={handleicon} className={styles.icons}>
+            <i  onClick={()=>icontoggle()} className={styles.icon} className="fas fa-heart"></i>
+          </div>
         </div>
       </Modal>
     </>
